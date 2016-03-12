3cb85d38cd5019daa70de1a3f3030c348c0d576d
hifitim <tim.hifi@gmail.com>
2016-03-11 20:24:49 -0600

    new version
    new

--- prob3.lisp
+++ prob3.lisp
@@ -1,8 +1,8 @@
 (defun prime-range (x n)
   (let ((n-sqrt (ceiling (sqrt n))))
     (cond ((> x n-sqrt) t)
-          ((eq (rem n x) 0) nil)
-          (t (prime-range (incf x) n)))))
+          ((zerop (rem n x)) nil)
+          (t (prime-range (+ x 1) n)))))
 
 (defun prime-num-p (x)
   (prime-range 2 x))
@@ -12,17 +12,21 @@
 
 (defun lpf-inner (x n n-sqrt)
   (let* ((x-div (/ n x)))
-    (cond ((eq 1 x) 1) ; no prime factor available
-          ((and (eq (rem n x) 0) 
+    (cond ((= 1 x) 1) ; no prime factor available
+          ((and (zerop (rem n x)) 
                 (prime-num-p x))
            x) ; x is the first and largest prime factor
           ((and (int-p x-div)
-                (> x-div x) 
-                (eq (rem n x-div) 0) 
                 (prime-num-p x-div))
            x-div) ; x-div is the first and largest prime factor
-          (t (lpf-inner (decf x) n n-sqrt)))))
+          (t (lpf-inner (- x 2) n n-sqrt)))))
 
 (defun largest-prime-factor (n)  
   (let* ((n-sqrt (ceiling (sqrt n))))
+    (if (zerop (rem n-sqrt 2))
+        (+ n-sqrt 1))
     (lpf-inner n-sqrt n n-sqrt)))
+
+;; Some large value test cases, with timing provided
+;(time (largest-prime-factor 9007199254740993))
+(time (largest-prime-factor 600851475143))

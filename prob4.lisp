17bd0da2edd019c4ed5d8cab75f60983a05cb1f2
Davis <tdavis@hpe.com>
2016-05-16 16:21:59 -0500

    solved problem 4

---
+++ prob4.lisp
@@ -0,0 +1,25 @@
+(defun is-palindrome (str)
+  (let ((r t))
+  (loop for i from 0 below (length str)
+      do
+      (if (not (eq
+            (aref str i)
+            (aref str (- (- (length str) 1) i))))
+          (setq r nil)))
+  (and r t)))
+
+(defun largest-palindrome (lo hi)
+  (let ((largest -1)
+        (product 0))
+    (loop for i from lo to hi
+          do
+          (loop for j from lo to hi
+                 do
+                 (setq product (* i j))
+                 (if (and (> product largest)
+                          (is-palindrome (write-to-string product)))
+                     (setq largest product))))
+    (format t "~a~%" largest)))
+
+(time (largest-palindrome 100 1000))
+
